4e80bf8dd36638270a2db20d173c8acdf8d29fcd
ok-coder1 <codershub.code@gmail.com>
2026-09-12 22:20:38 +0530

    test

--- Formula/snapx.rb
+++ Formula/snapx.rb
@@ -6,7 +6,7 @@ class Snapx < Formula
   sha256 "d85f4d2c3ea55ed5040f166fdf44ce03dd0270dc6ab05ee26771dbd02eaacc06"
   license "GPL-3.0-or-later"
   # Uncomment to bump the package when still using the same SnapX version. Acts like the release field in snapx.spec
-  revision 1
+  revision 2
   head "https://github.com/SnapXL/SnapX.git", branch: "develop"
 
   depends_on "dotnet" => :build
